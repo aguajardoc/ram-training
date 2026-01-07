97cbc080a765dd2b4e73db7a4dd79ffcca512df3
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-01-06 22:30:38 -0600

    Added session check

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,4 +1,12 @@
+"use client"
+import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
+
 function Leaderboard() {
+    const { data:session } = useSession();
+
+    if (!session) redirect("\sign-in")
+
     return (
         <>
         <h1>67</h1>
--- src/app/train/page.tsx
+++ src/app/train/page.tsx
@@ -1,11 +1,22 @@
 "use client"
 import { api } from "~/trpc/react";
 import Problem from "../_components/problem";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 function Train() {
     const { data: problems, isLoading } = api.problem.all.useQuery();
+    const { data: session } = useSession();
 
-    if (isLoading) return <></>;
+    if (!session) redirect("/sign-in")
+
+    if (isLoading) {
+        return (
+            <div className="loading">
+                Fetching cool problems, please wait!
+            </div>
+        )
+    }
 
     return (
     <div className="page">
