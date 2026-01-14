c362a7e403121dc75a90ed8cf2356b36d107791b
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-01-14 09:29:12 -0600

    Added search to user panel

--- src/app/_components/user-panel.tsx
+++ src/app/_components/user-panel.tsx
@@ -1,29 +1,53 @@
 import type { UserRow } from "../admin/types";
 import "../../styles/globals.css"
+import { useState } from "react";
 
 type Props = {
     users: UserRow[],
 };
 
 function UserPanel({ users } : Props) {
+    const [q, setQ] = useState("");
+        
+    const normalize = (s: string) =>
+    s.toLowerCase().replace(/[^\p{L}\p{N}]+/gu, "");
+
+    const filtered = users
+    .filter(u => {
+        const qn = normalize(q);
+        return (
+        normalize(u.name ?? "").includes(qn)
+        );
+    })
+    .slice(0, 5);
+
     return (
         <>
             <h1 className="title">
                 USERS
             </h1>
 
+            <h2 className="subtitle">
+                Search Users
+            </h2>
+
             {/* USER ACTIONS */}
 
                 {/* List Users */}
 
-                {users.map(u => (
-                    <div key={u.id}>
-                        {u.email}
+                {/* Search by email/name */}
+                <input
+                    type="text"
+                    placeholder="search users..."
+                    value={q}
+                    onChange={e => setQ(e.target.value)}
+                />
+
+                {filtered.map(u => (
+                    <div className="text" key={u.id}>
+                        {u.name}
                     </div>
                 ))}
-
-
-                {/* Search by email/name */}
                     {/* Solve Count */}
                     {/* Last Login */}
                     {/* Recent ACs */}
