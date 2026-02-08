61e4d7f84ad6ec6f7464997a7251c50efbbf2a34
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-02-08 14:31:40 -0600

    Added user addition to track

--- src/app/train/actions.ts
+++ src/app/train/actions.ts
@@ -2,18 +2,26 @@
 import { auth } from "~/server/auth";
 import { db } from "~/server/db";
 import { redirect } from "next/navigation";
-import type { ProblemDifficulty, ProblemType } from "generated/prisma";
 
-export async function enrollUserInTrack(formData: FormData) {
+export async function addUserToTrack(formData : FormData) {
   const session = await auth();
   if (!session || session.user.role !== "ADMIN") redirect("/");
 
-  const name = formData.get("name") as string;
-  const url = formData.get("url") as string;
+  const userId = formData.get("userId") as string;
+  const trackId = formData.get("trackId") as string;
 
-  if (!name || !url) return;
+  await db.userTrack.deleteMany({
+    where: {
+      userId: userId,
+    },
+  });
 
-  await db.problem.create({ data: { name, url } });
+  await db.userTrack.create({
+    data: {
+      userId,
+      trackId,
+    },
+  });
 
   redirect("/admin");
 }
