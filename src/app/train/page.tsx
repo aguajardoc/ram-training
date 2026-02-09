6b7afdfc8e5485868689c0196efbcd65ce6a35b2
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-02-08 20:00:03 -0600

    fixed client/server issues

--- src/app/train/page.tsx
+++ src/app/train/page.tsx
@@ -1,61 +1,19 @@
-import { api } from "~/trpc/react";
 import { auth } from "~/server/auth";
-import Problem from "../_components/problem";
-import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
-import LevelPicker from "../_components/level-picker";
 import { db } from "~/server/db";
-import "../../styles/level-picker.css"
-import "../../styles/globals.css"
-
+import TrainClient from "./train-client";
 
 async function Train() {
-    const { data: problems, isLoading } = api.problem.all.useQuery();
-    const { data: session } = useSession();
-    const { data: hasChosenLevel } = api.user.hasChosenLevel.useQuery();
+    const session = await auth();
+    if (!session) redirect("/sign-in");
 
-    if (!session) redirect("/sign-in")
     const levelMappings = await db.levelToTrack.findMany();
 
-    if (!hasChosenLevel) {
-        return (
-            <div>
-                <LevelPicker
-                    userId={session.user.id}
-                    mappings={levelMappings}
-                />
-            </div>
-        )
-    }
-
-    if (isLoading) {
-        return (
-            <div className="loading">
-                Fetching cool problems, please wait!
-            </div>
-        )
-    }
-
     return (
-    <div className="page">
-        {/* Title */}
-        <h1 className="title">
-            TRAINING
-        </h1>
-        {/* Choose Level */}
-
-        {/* Choose Mode */}
-
-        {/* Problem List */}
-        {problems?.map(p => (
-            <Problem
-                key={p.id}
-                problemType={p.type}
-                problemName={p.name}
-                problemURL={p.url}
-            />
-        ))}
-    </div>
+        <TrainClient
+            userId={session.user.id}
+            levelMappings={levelMappings}
+        />
     )
 }
 
---
+++ src/app/train/train-client.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { api } from "~/trpc/react";
+import Problem from "../_components/problem";
+import LevelPicker from "../_components/level-picker";
+import "../../styles/level-picker.css";
+import "../../styles/globals.css";
+
+type Props = {
+  userId: string;
+  levelMappings: any[];
+};
+
+export default function TrainClient({ userId, levelMappings }: Props) {
+  const { data: problems, isLoading } = api.problem.all.useQuery();
+  const { data: hasChosenLevel, isLoading: isCheckingLevel } = api.user.hasChosenLevel.useQuery();
+
+  if (isLoading || isCheckingLevel) {
+    return (
+      <div className="loading">
+        Fetching cool problems, please wait!
+      </div>
+    );
+  }
+
+  if (!hasChosenLevel) {
+    return (
+      <div>
+        <LevelPicker userId={userId} mappings={levelMappings} />
+      </div>
+    );
+  }
+
+  return (
+    <div className="page">
+      <h1 className="title">TRAINING</h1>
+      
+      {/* Problem List */}
+      {problems?.map((p) => (
+        <Problem
+          key={p.id}
+          problemType={p.type}
+          problemName={p.name}
+          problemURL={p.url}
+        />
+      ))}
+    </div>
+  );
+}
