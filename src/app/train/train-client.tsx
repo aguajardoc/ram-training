664bf9ff08c364c3ac8c963f250cb93ee9a79538
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-02-10 08:05:07 -0600

    Added separation by module

--- src/app/train/train-client.tsx
+++ src/app/train/train-client.tsx
@@ -1,15 +1,28 @@
 "use client";
 import { useState } from "react";
 import { api } from "~/trpc/react";
-import Problem from "../_components/problem";
 import LevelPicker from "../_components/level-picker";
 import "../../styles/level-picker.css";
 import "../../styles/globals.css";
+import type { ProblemDifficulty, ProblemType } from "generated/prisma";
+import { Fragment } from "react";
+import Problem from "../_components/problem";
 
 type Props = {
   userId: string,
   levelMappings: any[],
   enrolledTrackIds: string[],
+};
+
+type FullProblem = {
+  problem: { name: string; id: string; url: string; };
+  module: { name: string; };
+  id: string;
+  problemId: string;
+  moduleId: string;
+  order: number;
+  problemType: ProblemType;
+  difficulty: ProblemDifficulty;
 };
 
 export default function TrainClient({ userId, levelMappings, enrolledTrackIds }: Props) {
@@ -47,26 +60,52 @@ export default function TrainClient({ userId, levelMappings, enrolledTrackIds }:
     );
   }
 
+  // List of modules
+  // I care about modules and the problems they contain
+  const moduleMap = new Map<string, FullProblem[]>();
+
+  problems?.forEach(p => {
+    if (!moduleMap.has(p.moduleId)) {
+      moduleMap.set(p.moduleId, []);
+    }
+
+    moduleMap.get(p.moduleId)?.push(p);
+  });
+
+  const moduleList = Array.from(moduleMap, ([moduleId, problems]) => (
+    {
+      moduleId,
+      moduleName: problems[0]?.module.name,
+      problems: problems.sort((a, b) => a.order - b.order),
+    }
+  ));
+
   // Render problems available to user
   return (
     <div className="page">
       <h1 className="title">TRAINING</h1>
 
       {/* Module List */}
-      {/* map list of modules... */}
-
-          {/* Module Name */}
       
-          {/* Problem List */}
-          {problems?.map((p) => (
-            <Problem
-              key={p.id}
-              problemType={p.problemType}
-              problemName={p.problem.name}
-              problemURL={p.problem.url}
-              problemDifficulty={p.difficulty}
-            />
-          ))}
+      {moduleList.map(m => (
+        <div key={m.moduleId} className="module">
+          {/* Module Name */}
+          <h2 className="subtitle">
+            {m.moduleName}
+          </h2>
+          
+            {/* Problem List */}
+            {m.problems?.map((p) => (
+              <Problem
+                key={p.id}
+                problemType={p.problemType}
+                problemName={p.problem.name}
+                problemURL={p.problem.url}
+                problemDifficulty={p.difficulty}
+              />
+            ))}
+        </div>
+      ))}
 
       {/* Change Level */}
       <button 
