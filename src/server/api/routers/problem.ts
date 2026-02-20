febc3a04222803ec643e18935a3fccaff18527c0
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-02-20 12:18:23 -0600

    Created problem router procedures

--- src/server/api/routers/problem.ts
+++ src/server/api/routers/problem.ts
@@ -4,94 +4,123 @@ import {
   publicProcedure,
   protectedProcedure,
 } from "../trpc"
+import { createTextChangeRange } from "typescript"
 
 export const problemRouter = createTRPCRouter({
   all: publicProcedure.query(({ ctx }) => {
     return ctx.db.problem.findMany()
   }),
 
-  create: protectedProcedure
-    .input(
-      z.object({
-        type: z.string(),
-        name: z.string(),
-        url: z.string(),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      return ctx.db.problem.create({ data: input })
-    }),
+  upsert: protectedProcedure
+  .input(
+    z.object({
+      id: z.string().optional(),
+      name: z.string(),
+      url: z.string(),
+    })
+  )
+  .mutation(({ ctx, input }) => {
+    return ctx.db.problem.upsert({
+      where: {
+        id: input.id ?? "placeholder",
+      },
+      update: {
+        name: input.name,
+        url: input.url,
+      },
+      create: {
+        name: input.name,
+        url: input.url,
+      }
+    })
+  }),
 
-    getProblemsByTrack: protectedProcedure
-    .input(z.object({ trackId: z.string() }))
-    .query(async ({ ctx, input }) => {
-      return await ctx.db.moduleProblem.findMany({
-        where: {
-          module: {
-            levelModules: {
-              some: {
-                trackLevel: {
-                  trackId: input.trackId,
-                },
+  getProblemsByTrack: protectedProcedure
+  .input(z.object({ trackId: z.string() }))
+  .query(async ({ ctx, input }) => {
+    return await ctx.db.moduleProblem.findMany({
+      where: {
+        module: {
+          levelModules: {
+            some: {
+              trackLevel: {
+                trackId: input.trackId,
               },
             },
           },
         },
-        include: {
-          problem: {
-            include: {
-              solves: {
-                where: {
-                  userId: ctx.session.user.id
-                },
-                take: 1
-              }
+      },
+      include: {
+        problem: {
+          include: {
+            solves: {
+              where: {
+                userId: ctx.session.user.id
+              },
+              take: 1
             }
-          },
-          module: {
-            select: { name: true }
           }
         },
-        orderBy: [
-          { module: { id: 'asc' } },
-          { order: 'asc' }
-        ],
-      });
-    }),
+        module: {
+          select: { name: true }
+        }
+      },
+      orderBy: [
+        { module: { id: 'asc' } },
+        { order: 'asc' }
+      ],
+    });
+  }),
 
   submitSolve: protectedProcedure
-    .input(
-      z.object({
-        problemId: z.string(),
-        statusString: z.string(),
-        submitCount: z.number().int().default(0),
-        readTimeMinutes: z.number().int().default(0),
-        thinkTimeMinutes: z.number().int().default(0),
-        codeTimeMinutes: z.number().int().default(0),
-        debugTimeMinutes: z.number().int().default(0),
-        perceivedDifficulty: z.number().int().default(0),
-        onYourOwn: z.boolean(),
-      })
-    )
-    .mutation(async ({ ctx, input }) => {
-      const userId = ctx.session.user.id;
+  .input(
+    z.object({
+      problemId: z.string(),
+      statusString: z.string(),
+      submitCount: z.number().int().default(0),
+      readTimeMinutes: z.number().int().default(0),
+      thinkTimeMinutes: z.number().int().default(0),
+      codeTimeMinutes: z.number().int().default(0),
+      debugTimeMinutes: z.number().int().default(0),
+      perceivedDifficulty: z.number().int().default(0),
+      onYourOwn: z.boolean(),
+    })
+  )
+  .mutation(async ({ ctx, input }) => {
+    const userId = ctx.session.user.id;
 
-      const solve = await ctx.db.solve.upsert({
-        where: {
-          userId_problemId: {
-            userId: userId,
-            problemId: input.problemId,
-          },
-        },
-        create: {
+    const solve = await ctx.db.solve.upsert({
+      where: {
+        userId_problemId: {
           userId: userId,
-          ...input,
+          problemId: input.problemId,
         },
-        update: {
-          ...input,
-        },
-      });
+      },
+      create: {
+        userId: userId,
+        ...input,
+      },
+      update: {
+        ...input,
+      },
+    });
 
-      return solve;
-    }),
+    return solve;
+  }),
+
+
+  delete: protectedProcedure
+  .input(
+    z.object({
+      id: z.string(),
+    })
+  )
+  .mutation(async ({ ctx, input }) => {
+    ctx.db.problem.delete({
+      where: {
+        id: input.id,
+      }
+    })
+  }),
+  
 })
