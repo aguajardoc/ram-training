ceb13b6e802aafe9d4cfa6d9bc6b5840624e12a0
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-02-24 14:22:18 -0600

    Created script to convert vertices to faces

---
+++ src/app/modelling/utility/obj-to-vertices-faces.cpp
@@ -0,0 +1,161 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+struct Vertex {
+    float x, y, z;
+
+    Vertex(int _x, int _y, int _z) : x(_x), y(_y), z(_z) {}
+};
+
+struct Face {
+    std::vector<int> connected_indices;
+
+    Face(std::vector<int> _connected_indices) : connected_indices(_connected_indices) {}
+};
+
+bool is_vertex(std::string& file_line) {
+    return ((int)file_line.length() >= 2 and file_line[0] == 'v' and file_line[1] == ' ');
+}
+
+bool is_face(std::string& file_line) {
+    return ((int)file_line.length() >= 2 and file_line[0] == 'f' and file_line[1] == ' ');
+}
+
+Vertex parse_vertex(std::string& file_line) {
+    // Consists of 3 real numbers: x, y, z
+    float values[3] = {0, 0, 0};
+
+    // Appended a space to handle all logic within the for loop
+    file_line += ' ';
+
+    // Keep current value and current value index
+    std::string current_value;
+    int current_index = 0;
+
+    for (int i = 2; i <= (int)file_line.size(); i++) {
+        // If it's a space, we have either finished a number or encountered filler
+        if (file_line[i] == ' ') {
+            // Filters for possible filler
+            if (!current_value.empty()) {
+                values[current_index++] = stof(current_value);
+                current_value.clear();
+            }
+        }
+        else {
+            current_value += file_line[i];
+        }
+    }
+
+    return Vertex(values[0], values[1], values[2]);
+}
+
+Face parse_face(std::string& file_line) {
+    // Consists of at least 3 integers, as indices
+    std::vector<int> indices;
+    // They can be separated by / to indicate different attachments
+    // In this case, we only care about the first integer
+
+    // Appended a space to handle all logic within the for loop
+    file_line += ' ';
+
+    // Keep current value
+    std::string current_value;
+
+    for (int i = 2; i <= (int)file_line.size(); i++) {
+        // If it's a space, we have either finished a number or encountered filler
+        if (file_line[i] == ' ') {
+            // Filters for possible filler
+            if (!current_value.empty()) {
+                // If it has forward slashes, break the number until the first one
+                auto it = std::find(current_value.begin(), current_value.end(), '/');
+
+                if (it != current_value.end()) {
+                    current_value = current_value.substr(0, it - current_value.begin()); 
+                }
+
+                // Add to vector and clear number string
+                indices.push_back(stoi(current_value) - 1); // .obj file face indices are 1-indexed; we want 0-indexed
+                current_value.clear();
+            }
+        }
+        else {
+            current_value += file_line[i];
+        }
+    }
+
+    return Face(indices);
+}
+
+void to_json(json& j, const Vertex& v) {
+    j = json{{"x", v.x}, {"y", v.y}, {"z", v.z}};
+}
+
+void to_json(json& j, const Face& f) {
+    j = f.connected_indices;
+}
+
+int main() {
+    // Get input file name
+    std::string input_file_name;
+    std::cout << "Input .obj file name (case-sensitive, without file extension): ";
+    std::cin >> input_file_name;
+    std::cout << '\n';
+
+    // Get input file path
+    std::string input_path = "../raw-models/";
+    input_path += input_file_name;
+    input_path += ".obj";
+
+    // Validate file existence, while opening the file
+    if (std::freopen(input_path.c_str(), "r", stdin)) {
+        std::cout << "A file exists! Starting vertex + face extraction...\n"; 
+    }
+    else {
+        std::cerr << "A file with this name and .obj extension was not found in this folder. Exiting...\n";
+        return -1;
+    }
+
+    // Extract vertices and faces
+    std::string file_line;
+    std::vector<Vertex> vertices;
+    std::vector<Face> faces;
+
+    while (getline(std::cin, file_line)) {
+        // Check if vertex
+        if (is_vertex(file_line)) {
+            vertices.push_back(parse_vertex(file_line));
+        }
+        // Check if face
+        else if (is_face(file_line)) {
+            faces.push_back(parse_face(file_line));
+        }
+        // Else it is ignored
+    }
+
+    // Write to JSON
+    json model;
+    model["vs"] = vertices;
+    model["fs"] = faces; 
+
+    std::cout << "Extraction complete!\n";
+    std::cout << "\nSaving model to the " << input_file_name << ".json file...\n";
+
+    // Format file path
+    std::string output_path = "../models/";
+    output_path += input_file_name;
+    output_path += ".json";
+
+    // Write to path
+    std::ofstream file(output_path);
+    file << model.dump(4); // 4 => indentation
+
+    std::cout << "Model saved! Exiting...\n";
+
+    return 0;
+}
